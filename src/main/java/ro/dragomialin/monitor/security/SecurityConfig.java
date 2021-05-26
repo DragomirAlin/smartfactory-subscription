88d21b07ee9313acf231bed51c72a865a8c0dc86
dragomiralin <dragomirdanielalin@yahoo.com>
2021-05-26 22:33:50 +0300

    add SecurityConfig

---
+++ src/main/java/ro/dragomialin/monitor/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package ro.dragomialin.monitor.security;
+
+import feign.RequestInterceptor;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+        http.cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/me", "/api/foos/**")
+                .hasAuthority("SCOPE_read")
+                .antMatchers(HttpMethod.POST, "/api/foos")
+                .hasAuthority("SCOPE_write")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }// @formatter:on
+
+    @Bean
+    JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri()).build();
+        jwtDecoder.setClaimSetConverter(new OrganizationSubClaimAdapter());
+        return jwtDecoder;
+    }
+
+    @Bean
+    public RequestInterceptor authRequestInterceptor() {
+        return requestTemplate -> {
+            var jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            requestTemplate.header("Authorization", String.format("Bearer %s", jwt.getTokenValue()));
+        };
+    }
+}
+
