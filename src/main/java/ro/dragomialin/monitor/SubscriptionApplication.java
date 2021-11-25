cc37b4dc790ebbb6c804a9b4caacffa264c3559f
dragomiralin <dragomirdanielalin@yahoo.com>
2021-11-25 21:20:59 +0200

    add consul

--- src/main/java/ro/dragomialin/monitor/SubscriptionApplication.java
+++ src/main/java/ro/dragomialin/monitor/SubscriptionApplication.java
@@ -2,7 +2,9 @@ package ro.dragomialin.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SubscriptionApplication {
 	public static void main(String[] args) {
