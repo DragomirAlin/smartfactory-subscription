aa0118875d2f712d84f3099d7b380c7a03ceaf10
dragomiralin <dragomirdanielalin@yahoo.com>
2022-05-07 15:56:02 +0300

    delete label

--- src/main/java/ro/dragomialin/monitor/SubscriptionApplication.java
+++ src/main/java/ro/dragomialin/monitor/SubscriptionApplication.java
@@ -2,9 +2,7 @@ package ro.dragomialin.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class SubscriptionApplication {
 	public static void main(String[] args) {
