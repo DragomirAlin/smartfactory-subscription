f771af3be645d96fc331edf529445c1256ca9100
dragomiralin <dragomirdanielalin@yahoo.com>
2022-06-04 16:21:06 +0300

    add annotation

--- src/main/java/ro/dragomialin/monitor/SubscriptionApplication.java
+++ src/main/java/ro/dragomialin/monitor/SubscriptionApplication.java
@@ -2,8 +2,10 @@ package ro.dragomialin.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class SubscriptionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SubscriptionApplication.class, args);
